7b5490f12520b0f1b69ee99fd181c5b597c6bb76
Eric <123MegaApple123@gmail.com>
2017-11-26 18:04:20 -0600

    begin v2

--- js/index.js
+++ js/index.js
@@ -1,96 +1,69 @@
-/* globals document, window, XMLHttpRequest, nodeRequire, Sizzle, TriangleBG */
-(function(){
-  'use strict';
+/* jshint browser: true */
+(function () {
+  "use strict";
 
-  const {clipboard} = nodeRequire('electron');
-
-  const $ = Sizzle;
+  function $(selector) {
+    return document.querySelectorAll(selector);
+  }
 
   const $inputText = $('input[type="text"]')[0];
-  const $webview = $('webview')[0];
   const $loading = $('loading')[0];
   const $snackbar = $('snackbar')[0];
   const $markdown = $('markdown')[0];
 
-  $inputText.addEventListener('keydown', function(e){
-    if( !$snackbar.hasAttribute('hidden') ){
+  $inputText.addEventListener('keydown', function (e) {
+    if (!$snackbar.hasAttribute('hidden')) {
       $snackbar.setAttribute('hidden', '');
     }
-    if( e.key === "Enter" ){
+    if (e.key === "Enter") {
       let value = this.value;
-      if( this.checkValidity() && value.length > 2 ){
-        $webview.setAttribute('src', "https://osu.ppy.sh/users/" + window.encodeURIComponent(value));
+      if (this.checkValidity() && value.length > 2) {
         this.setAttribute('disabled', '');
         $loading.removeAttribute('hidden');
-      }else{
+
+        fetch(`https://osu.ppy.sh/users/${window.encodeURIComponent(value)}`, {
+          "method": "HEAD",
+          "headers": new Headers({
+            "Access-Control-Allow-Origin": "*"
+          }),
+          "mode": "cors",
+          "cache": "default",
+          "credentials": "omit"
+        }).then(function (e) {
+          console.log(e);
+        }).catch(function (e) {
+          console.log(e);
+        });
+      } else {
         $snackbar.setAttribute('data-state', 'empty');
         $snackbar.removeAttribute('hidden');
       }
     }
   }, false);
-  $webview.addEventListener('did-stop-loading', function(){
-    let state;
-    let id;
-    try{
-      //id = $webview.src.match(/[0-9]*$/)[0];
-      id = $webview.src;
-      id = id.substring(id.lastIndexOf("/") + 1, id.length);
-      state = "passed";
-    }catch(e){
-      state = "failed";
-    }finally{
-      if( id.length > 0 && !Number.isNaN(Number(id)) ){
-        if( $markdown.getAttribute('data-state') === 'enabled' ){
-          let title = $webview.getTitle().replace(/(\'s profile)$/, "");
-          id = "[" + title + "](https://osu.ppy.sh/users/" + id + ")";
-        }
-        clipboard.writeText(id);
-      }else{
-        state = "empty";
+  /*$webview.addEventListener('did-stop-loading', function () {
+  let state;
+  let id;
+  try {
+    //id = $webview.src.match(/[0-9]*$/)[0];
+    id = $webview.src;
+    id = id.substring(id.lastIndexOf("/") + 1, id.length);
+    state = "passed";
+  } catch (e) {
+    state = "failed";
+  } finally {
+    if (id.length > 0 && !Number.isNaN(Number(id))) {
+      if ($markdown.getAttribute('data-state') === 'enabled') {
+        let title = $webview.getTitle().replace(/(\'s profile)$/, "");
+        id = "[" + title + "](https://osu.ppy.sh/users/" + id + ")";
       }
-      $loading.setAttribute('hidden', '');
-      $inputText.removeAttribute('disabled');
-      $snackbar.setAttribute('data-state', state);
-      $snackbar.removeAttribute('hidden');
+      //clipboard.writeText(id);
+    } else {
+      state = "empty";
     }
-  });
-  $markdown.addEventListener('click', function(){
-    if( this.getAttribute('data-state') === 'enabled' ){
-      this.setAttribute('data-state', 'disabled');
-    }else{
-      this.setAttribute('data-state', 'enabled');
-    }
-  });
-
-  /* triangle.js stuff */
-
-  let $canvas = $('canvas')[0];
-  let $altBG = $('alt-canvas')[0];
-  let triangle = new TriangleBG({
-    canvas: $canvas,
-    alternateElem: $altBG,
-    cellHeight: 120,
-    cellWidth: 100,
-    mouseLight: true,
-    mouseLightRadius: 500,
-    mouseLightIncrement: 10,
-    resizeAdjustment: true,
-    variance: 1.3,
-    pattern: "x*y",
-    baseColor1: {
-      baseHue: 339,
-      baseSaturation: 81,
-      baseLightness: 58
-    },
-    baseColor2:{
-      baseHue: 339,
-      baseSaturation: 82,
-      baseLightness: 66
-    },
-    colorDelta:{
-      hue: 1,
-      lightness: 0,
-      saturation: 0
-    }
-  });
+    $loading.setAttribute('hidden', '');
+    $inputText.removeAttribute('disabled');
+    $snackbar.setAttribute('data-state', state);
+    $snackbar.removeAttribute('hidden');
+  }
+});*/
 })();
